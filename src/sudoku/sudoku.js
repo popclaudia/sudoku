383ef01551673891aabdf00ede744ca534aecb42
Popclaudia <pop_claudia_dorina@yahoo.com>
2021-09-02 12:05:57 +0300

    final sudoku

--- src/sudoku/sudoku.js
+++ src/sudoku/sudoku.js
@@ -10,29 +10,31 @@ class Sudoku extends React.Component {
         this.handleClick = this.handleClick.bind(this);
         this.state = {
             solution: [
-                [8, 3, 5, 4, 1, 6, 9, 2, 7],
-                [2, 9, 6, 8, 5, 7, 4, 3, 1],
-                [4, 1, 7, 2, 9, 3, 6, 5, 8],
-                [5, 6, 9, 1, 3, 4, 7, 8, 2],
-                [1, 2, 3, 6, 7, 8, 5, 4, 9],
-                [7, 4, 8, 5, 2, 9, 1, 6, 3],
-                [6, 5, 2, 7, 8, 1, 3, 9, 4],
-                [9, 8, 1, 3, 4, 5, 2, 7, 6],
-                [3, 7, 4, 9, 6, 2, 8, 1, 5]
+                [8, 3, 5, 2, 9, 6, 4, 1, 7],
+                [4, 1, 6, 8, 5, 7, 2, 9, 3],
+                [9, 2, 7, 4, 3, 1, 6, 5, 8],
+                [5, 6, 9, 1, 2, 3, 7, 4, 8],
+                [1, 3, 4, 6, 7, 8, 5, 2, 9],
+                [7, 8, 2, 5, 4, 9, 1, 6, 3],
+                [6, 5, 2, 9, 8, 1, 3, 7, 4],
+                [7, 8, 1, 3, 4, 5, 9, 6, 2],
+                [3, 9, 4, 2, 7, 6, 8, 1, 5]
             ],
             current: [
-                [8, 0, 5, 4, 1, 6, 0, 2, 7],
-                [2, 9, 0, 0, 5, 0, 4, 3, 1],
-                [4, 0, 0, 2, 0, 3, 0, 5, 0],
-                [0, 6, 9, 1, 3, 0, 0, 8, 2],
-                [1, 0, 0, 6, 7, 8, 0, 4, 0],
-                [7, 4, 8, 5, 2, 0, 1, 6, 3],
-                [6, 0, 0, 7, 8, 0, 0, 9, 4],
-                [0, 8, 1, 3, 0, 5, 2, 0, 6],
-                [3, 7, 4, 0, 6, 2, 8, 0, 5]
+                [8, 0, 5, 2, 9, 6, 0, 1, 7],
+                [0, 1, 0, 8, 5, 7, 0, 9, 3],
+                [0, 2, 7, 0, 3, 1, 6, 0, 8],
+                [5, 6, 9, 0, 2, 0, 7, 4, 8],
+                [0, 3, 4, 6, 7, 0, 0, 2, 9],
+                [7, 8, 2, 5, 0, 9, 0, 6, 3],
+                [0, 5, 2, 9, 8, 0, 0, 7, 4],
+                [7, 0, 1, 0, 4, 0, 0, 6, 2],
+                [3, 0, 4, 0, 7, 0, 8, 1, 0]
             ],
             selectedCell1: null,
             selectedCell9: null,
+            clickedCell: null,
+            correctValues: 0,
         };
     }
 
@@ -44,6 +46,7 @@ class Sudoku extends React.Component {
         this.setState({
             selectedCell1: c1,
             selectedCell9: c9,
+            clickedCell: e.target,
         });
         console.log(c1, c9)
 
@@ -62,20 +65,46 @@ class Sudoku extends React.Component {
     }
 
     updateSudoku(i) {
-        const curr = this.state.current.slice();
-        // curr[this.state.selectedCell9][this.state.selectedCell1] = i;
-        console.log(this.state.current[this.state.selectedCell9][this.state.selectedCell1] );
-        console.log(this.state.solution[this.state.selectedCell9][this.state.selectedCell1] );
-        
+
+        console.log(this.state.current[this.state.selectedCell9][this.state.selectedCell1]);
+        console.log(this.state.solution[this.state.selectedCell9][this.state.selectedCell1]);
         if (this.state.current[this.state.selectedCell9][this.state.selectedCell1] != this.state.solution[this.state.selectedCell9][this.state.selectedCell1]) {
+            const curr = this.state.current.slice();
+            curr[this.state.selectedCell9][this.state.selectedCell1] = i;
+            console.log(this.state.current[this.state.selectedCell9][this.state.selectedCell1]);
+            console.log(this.state.solution[this.state.selectedCell9][this.state.selectedCell1]);
             this.setState({
                 current: curr,
             })
+            this.state.clickedCell.style.color = 'darkred'
+            if (this.state.current[this.state.selectedCell9][this.state.selectedCell1] == this.state.solution[this.state.selectedCell9][this.state.selectedCell1]) {
+                this.state.clickedCell.style.color = 'darkgreen'
+
+                let count = 0;
+                for (let i = 0; i < 9; i++)
+                    for (let j = 0; j < 9; j++) {
+                        if (this.state.current[i][j] == this.state.solution[i][j]) {
+                            count++;
+                        }
+                    }
+                this.setState({
+                    correctValues: count
+                })
+            }
         }
+
+
+
 
     }
 
     render() {
+
+        const isWon = () => {
+            if (this.state.correctValues == 81) {
+                return <h1>You won!</h1>
+            }
+        }
 
         return (
             <div>
@@ -106,6 +135,12 @@ class Sudoku extends React.Component {
                         {this.render9Cell(7)}
                         {this.render9Cell(8)}
                     </div>
+                </div>
+
+                <div>
+                    {
+                        isWon()
+                    }
                 </div>
             </div>
         );
